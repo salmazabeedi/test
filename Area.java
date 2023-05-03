3e1c678f574e991cc0a41f422e0d979bf95f6b79
lamanaif12 <132441635+lamanaif12@users.noreply.github.com>
2023-05-03 11:45:16 +0300

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package area;
+
+/**
+ *
+ * @author abodn
+ */
+public class Area {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        area1.getArea(3, 4);
+    }
+    
+}
